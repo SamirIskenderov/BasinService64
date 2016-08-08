2832508763537dacdf8ceedc1ed32ae6f7fcca45
Samir Iskenderov <Samir_Iskenderov@epam.com>
2016-08-08 10:50:13 +0300

    Init

---
+++ BasinService64.Contracts/Dal/IBaseProductDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasinService64.Dtos;
+
+namespace BasinService64.Contracts.Dal
+{
+    public interface IBaseProductDal
+    {
+        List<BaseProduct> GetProducts(BaseFilter filter, out int numberOfPages);
+    }
+}
---
+++ BasinService64.Dtos/Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasinService64.Dtos
+{
+    public class Service : BaseProduct
+    {
+    }
+}
