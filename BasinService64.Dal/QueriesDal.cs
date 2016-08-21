4922c6e6268ebb8cd108956545b8296dbf6a96c5
DESKTOP-N9NDU1S\Samir <Samir_Iskenderov@epam.com>
2016-08-21 12:51:06 +0300

    11

---
+++ BasinService64.Dal/QueriesDal.cs
@@ -0,0 +1,92 @@
+﻿using BasinService64.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasinService64.Dal
+{
+    public class QueriesDal
+    {
+        public List<QueryDto> GetAllQueries()
+        {
+            var result = new List<QueryDto>();
+            var item = new QueryDto();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString))
+            {
+                const string query = "SELECT [Id], [Name], [Message], [PhoneNumber], [Email], [Date] FROM [Queries] ORDER BY [Date] DESC;";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new QueryDto()
+                            {
+                                ID = (int)reader["Id"],
+                                Message = (string)reader["Message"],
+                                Name = (string)reader["Name"],
+                                PhoneNumber = (string)reader["PhoneNumber"],
+                                Email = (string)reader["Email"],
+                                Date = (DateTime)reader["Date"],
+                            });
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void AddQuery(QueryDto item)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString))
+            {
+                const string procedureName = "AddQuery";
+
+                using (var command = new SqlCommand(procedureName, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    SqlParameter RetId = command.Parameters.Add("RetVal", SqlDbType.Int);
+                    RetId.Direction = ParameterDirection.ReturnValue;
+
+                    command.Parameters.AddWithValue("@Name", item.Name);
+                    command.Parameters.AddWithValue("@Message", item.Message);
+                    command.Parameters.AddWithValue("@PhoneNumber", item.PhoneNumber);
+                    command.Parameters.AddWithValue("@Email", item.Email);
+                    command.Parameters.AddWithValue("@Date", item.Date);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    item.ID = (int)RetId.Value;
+                }
+            }
+        }
+
+        public void DeleteQuery(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString))
+            {
+                const string query = "DELETE FROM [Queries] WHERE [Id] = @Id;";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id.ToString());
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ BasinSevice64.WebPages/Model/SolutionHelper.cs
@@ -0,0 +1,58 @@
+﻿using BasinService64.Dal;
+using BasinService64.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace BasinSevice64.WebPages.Model
+{
+    public static class SolutionHelper
+    {
+        public static QueriesDal QDal { get; } = new QueriesDal();
+
+        public static void TrySendEmail(QueryDto query)
+        {
+            if (string.IsNullOrWhiteSpace(query.Name))
+            {
+                query.Name = "Не указано.";
+            }
+            if (string.IsNullOrWhiteSpace(query.Message))
+            {
+                query.Message = "Не указано.";
+            }
+            if (string.IsNullOrWhiteSpace(query.Email))
+            {
+                query.Email = "Не указано.";
+            }
+            if (string.IsNullOrWhiteSpace(query.PhoneNumber))
+            {
+                query.PhoneNumber = "Не указано.";
+            }
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("socnet.iskenderov.epam@gmail.com");
+                mail.To.Add(new MailAddress("pr0gy@bk.ru"));
+                mail.Subject = "New BasinService Query";
+                String mailbody = string.Format("Добрый день!{0}На сайте оставлен запрос от {1}.{0}Контактные данные:{0}Телефон: {2}{0}Электронная почта: {3}{0}Оставленное сообщение: {4}{0}Дата: {5}", Environment.NewLine, query.Name, query.PhoneNumber, query.Email, query.Message, query.Date);
+                mail.Body = mailbody;
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("socnet.iskenderov.epam", "d8gg9Hr7TH64K");
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mail);
+                mail.Dispose();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}
